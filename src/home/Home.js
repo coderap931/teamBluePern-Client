2f303bb581496403f65f8b030a80d0e2ddad95a4
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-11-29 17:42:57 -0500

    initial organization setup for React components with Jaylen and Adam. will reconvene tuesday night to see our progress so far for client-side. comments within code show components responsibility.

---
+++ src/home/Home.js
@@ -0,0 +1,3 @@
+import Sidebar from './home/Sidebar'; //Sidebar | Likely being moved into HomePage. //!Alex
+import GameViewModal from './games/GameViewModal'; //Game View Modal with Single Game's Info //!Jaylen
+
