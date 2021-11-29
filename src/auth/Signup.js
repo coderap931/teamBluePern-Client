3a48e1653c216c5d4411942dddcb14bf150911a4
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-11-29 17:58:38 -0500

    added base React component templates to each compents for all files so teammates could pull and start quickly.

---
+++ src/auth/Signup.js
@@ -0,0 +1,15 @@
+//* Insert imports here
+import { UseState } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+
+const Signup = (props) => {
+    //* functions replaced here
+
+    return (
+        <div className="signup-page">
+            <SignupForm />
+        </div>
+    );
+}
+
+export default Signup;
---
+++ src/games/GameViewModal.js
@@ -0,0 +1,12 @@
+//* GameViewModal imports replaced here
+
+const GameViewModal = (props) => {
+    //* functions replaced here
+
+    return (
+        <div className="game-view-modal">
+        </div>
+    )
+}
+
+export default GameViewModal;
