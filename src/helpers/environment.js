d586d4ec76dee8020efc139e714b8d037324abc1
coderap931 <88391040+coderap931@users.noreply.github.com>
2021-12-04 16:30:08 -0500

    changed stuff

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5432';
         break;
-    case 'amp-my-gamechest.herokuapp.com':
+    case 'amp-my-gamechest-react.herokuapp.com':
         APIURL = 'https://amp-my-gamechest.herokuapp.com'
 }
 
