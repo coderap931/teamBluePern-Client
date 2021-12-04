3b37673f6854e03f6c20f7cf4fd1b4110f0b3540
coderap931 <88391040+coderap931@users.noreply.github.com>
2021-12-04 15:58:33 -0500

    fixing things the modules said nothing about

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5432';
         break;
-    case 'amp-my-gamechest-react.herokuapp.com':
-        APIURL = 'https://amp-my-gamechest-react.herokuapp.com'
+    case 'amp-my-gamechest.herokuapp.com':
+        APIURL = 'https://amp-my-gamechest.herokuapp.com'
 }
 
 export default APIURL;
