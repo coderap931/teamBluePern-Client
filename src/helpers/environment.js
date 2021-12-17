0107394a2ff67c795c8e4868f100494ac5163209
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-12-17 15:30:08 -0500

    added easy comment/uncomment for localhost in helpers/environment.js. make sure to always uncomment the heroku APIURL before committing and merging or else Heroku won't work! merging develop down to my personal branch Alex from here.

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,6 +1,8 @@
 /* eslint-disable default-case */
 let APIURL = '';
 
+// TODO MANDATORY: Uncomment lines 6 through 12 for Heroku deployment.
+//! UNCOMMENT FOR HEROKU DEPLOYMENT
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5432';
@@ -9,5 +11,8 @@ switch (window.location.hostname) {
         APIURL = 'https://amp-my-gamechest.herokuapp.com'
 }
 
+// TODO MANDATORY: Comment line 16 out!
+//! Comment line 16 out before committing. Uncomment Heroku URL before committing in lines 4 through 10.
+// APIURL = "http://localhost:3000"
 
 export default APIURL;
